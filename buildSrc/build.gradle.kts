fb86f2fe7c11bf02a0f9f7f27835aad150c98253
ygdrasil-io <alexandre.mommers@gmail.com>
2024-09-08 17:55:31 +0200

    Initial commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+	`kotlin-dsl`
+}
+
+
+repositories {
+	gradlePluginPortal()
+	google()
+	mavenCentral()
+}
+
+dependencies {
+	implementation(libs.download)
+	implementation(libs.kotlin.multiplatform)
+	implementation(libs.jreleaser.plugin)
+	implementation(libs.android.library)
+
+	implementation(libs.gson)
+	implementation(libs.zip4j)
+	implementation(libs.okhttp)
+	implementation(libs.commons.io)
+}
